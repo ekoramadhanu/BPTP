986ba7d9d6244e7657443908dfad64b91d7bb9ac
Eko Rama <ekorama97@student.ub.ac.id>
2019-07-17 14:38:47 +0700

    penambahan bantuan

--- application/views/Bantuan/alur_penerimaan.php
+++ application/views/Bantuan/alur_penerimaan.php
@@ -16,32 +16,32 @@
             </li>
             <li>Cari nama peserta magang yang ingin anda tindak lanjuti pada tabel daftar magang</li>
             <li>Atau anda bisa melakukan pencarian dengan memasukkan nama/tahun/keduanya
-            <br>
-            <img src="<?=base_url('Assets/image/')?>pencarianDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
+              <br>
+              <img src="<?=base_url('Assets/image/')?>pencarianDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
             </li>
             <li>Pilih tomboh “Cari”
-            <br>
-            <img src="<?=base_url('Assets/image/')?>tombolCariDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
+              <br>
+              <img src="<?=base_url('Assets/image/')?>tombolCariDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
             </li>
             <li>Jika sudah menemukan nama peserta, pilih tombol dengan ikon suka untuk menerima peserta magang
-            <br>
-            <img src="<?=base_url('Assets/image/')?>setujuDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
+              <br>
+              <img src="<?=base_url('Assets/image/')?>setujuDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
             </li>
             <li>Jika setuju, masukkan nama pembimbing magang di kolom pembimbing
-            <br>
-            <img src="<?=base_url('Assets/image/')?>mengisiPembimbingDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
+              <br>
+              <img src="<?=base_url('Assets/image/')?>mengisiPembimbingDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
             </li>
             <li>Jika setuju, masukkan penempatan magang di kolom penempatan magang
-            <br>
-            <img src="<?=base_url('Assets/image/')?>mengisiPenempatanMagangDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
+              <br>
+              <img src="<?=base_url('Assets/image/')?>mengisiPenempatanMagangDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
             </li>
             <li>Pilih tombol setuju
-            <br>
-            <img src="<?=base_url('Assets/image/')?>setujuDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
+              <br>
+              <img src="<?=base_url('Assets/image/')?>setujuDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
             </li>
             <li>Jika tidak menyetujui, maka piih tombol dengan ikon tidak suka
-            <br>
-            <img src="<?=base_url('Assets/image/')?>tolakDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
+              <br>
+              <img src="<?=base_url('Assets/image/')?>tolakDaftarMagang.png" class="mt-1 mb-4 img-fluid img-thumbnail">
             </li>
             
         </ol>
