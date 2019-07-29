56ee23f0d2b75fa7818a0a97924a81473e7a3f4a
Eko Rama <ekorama97@student.ub.ac.id>
2019-07-29 10:38:42 +0700

    perbaikan kata

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -24,7 +24,7 @@ class Admin extends CI_Controller {
             // validasi form
             $this->form_validation->set_rules('username','username','is_unique[user.username]|alpha_dash',[
                 'is_unique'=>'Nama Pengguna Sudah Ada Silahkan Cari yang Lainnya',
-                'alpha_dash'=>'Nama Pengguna Hanya Boleh Huruf,Angka, dan Underscore'
+                'alpha_dash'=>'Nama Pengguna Hanya Boleh Huruf, spasi, Angka, dan Underscore'
             ]);
             $data['title'] = 'Daftar Administrator';
             $role['title'] = $data['title'];
--- application/controllers/Magang.php
+++ application/controllers/Magang.php
@@ -153,9 +153,7 @@ class Magang extends CI_Controller {
         $detail['faculty']=$this->Internship->getFakultasByKelompok($kelompok);
         $detail['detail'] = $this->Internship->getRekapBalasanByKelompok($kelompok);	
         $nomorSurat=$detail['nomorSurat']."/HM.240/H.12.15/".$detail['bulan']."/".$detail['tahun'];             
-        $result= $this->Internship->updateNomorSuratByIdKelompok($nomorSurat,$kelompok);
-        // print_r($detail['detail']);
-        // die();
+        $result= $this->Internship->updateNomorSuratByIdKelompok($nomorSurat,$kelompok);        
 		$this->load->view('print_balasan',$detail);
 	}
 
